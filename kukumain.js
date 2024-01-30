dc84203c5823c6e82a144306a5c9d90dc300a66c
Mimiview <57045037+Mimiview@users.noreply.github.com>
2024-01-30 20:14:10 +0100

    scripts added
    start
    commands

--- kukumain.js
+++ kukumain.js
@@ -2,9 +2,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
-
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
 const token = process.env.TOKEN;
 
 console.log(token)
